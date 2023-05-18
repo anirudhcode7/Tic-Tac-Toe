82609fb2d70bc008bc99de35e0862c7fe717df27
anirudh.ponna <anirudh.ponna@appviewx.com>
2023-05-18 22:34:22 +0530

    First Version: Working Model

---
+++ script.js
@@ -0,0 +1,60 @@
+let board = Array.from(document.getElementsByClassName('cell'));
+let o_turn = false;
+
+function checkForVictory(squares) {
+  let combos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  return combos.find(combo => {
+    if (
+      squares[combo[0]].innerText &&
+      squares[combo[0]].innerText === squares[combo[1]].innerText &&
+      squares[combo[0]].innerText === squares[combo[2]].innerText
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+}
+
+board.forEach(cell => {
+  cell.addEventListener('click', handleClick, { once: true });
+});
+
+function handleClick(e) {
+  if (o_turn) {
+    e.target.innerText = 'O';
+    e.target.style.color = 'red';
+  } else {
+    e.target.innerText = 'X';
+    e.target.style.color = 'blue';
+  }
+
+  if (checkForVictory(board)) {
+    setTimeout(() => {
+      alert('We have a winner!');
+      resetBoard();
+    }, 100);
+  } else {
+    o_turn = !o_turn;
+  }
+}
+
+document.getElementById('reset').addEventListener('click', resetBoard);
+
+function resetBoard() {
+  board.forEach(cell => {
+    cell.innerText = '';
+    cell.addEventListener('click', handleClick, { once: true });
+  });
+  o_turn = false;
+}
