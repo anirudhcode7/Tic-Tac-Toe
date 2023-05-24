6e3d18d760931b6971ad5c18965bf9d0df0ac3c3
anirudh.ponna <anirudh.ponna@appviewx.com>
2023-05-24 13:46:13 +0530

    Bug Fix: Fixed initial spacing

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ function handleClick(e) {
     let randomIndex = Math.floor(Math.random() * funWinningPhrases.length);
     // status.innerText = funWinningPhrases[randomIndex];
     typeStatus(funWinningPhrases[randomIndex]);
-    // setRandomQuote();
+    setRandomQuote();
 
   } else if (board.every(cell => cell.innerText !== '')) { // Check for a draw
     gameEnded = true;
